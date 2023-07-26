d38a19cbcd9c01ee6f10bd373c5925b5aa3b24b6
Calvin Kong <calvinxiah3128@hotmail.com>
2023-07-26 15:15:17 +1000

    Imported 2.2P

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 let express = require('express');
 let app = express();
-let port = 3000;
+let port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/', (req, res) => {
-    res.json('index.html');
+app.get('/',(req,res)=>{
+    res.render('index.html');
 });
 
-app.listen(port, () => {
-    console.log("Server running, listenting to port: " + port);
+app.get('/addTwoNumbers',(req,res)=>{
+    let num1 = req.query.number1; // is 1
+    let num2 = req.query.number2; // is 2
+    let result = parseInt(num1) + parseInt(num2);
+
+    let code = 200;
+    let msg = 'sucess';
+
+    let resObj = {data:result,statusCode:code,message:msg}
+
+    // '1' vs 1
+    res.json(resObj);
+});
+
+app.listen(port, ()=>{
+    console.log('server started, listening to port: ' + port);
 });
